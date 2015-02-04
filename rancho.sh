050068053ddff8d80e09e03f1ad029d12cf5f918
Craig <ceaveson@gmail.com>
2015-02-04 21:48:31 +0000

    Working random choice generator

--- rancho.sh
+++ rancho.sh
@@ -1,1 +1,15 @@
 #! /bin/usr/python
+
+from sys import argv
+from random import choice
+
+keylist = []
+choices = {}
+key = 0
+
+for arg in argv[1:]:
+    key += 1
+    keylist.append(key)
+    choices[key] = arg
+    
+print choices[choice(keylist)]
